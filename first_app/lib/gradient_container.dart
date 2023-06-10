0fe372af0a41fdd560afd2ab919e7a1005989e9f
Arnab Ghosh <as.920037arnabghosh@gmail.com>
2023-06-10 19:26:51 +0530

    Adding button

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,8 +1,13 @@
+import 'package:first_app/style_text.dart';
 import 'package:flutter/material.dart';
 // import 'package:first_app/style_text.dart' ;
 
 class GradientContiner extends StatelessWidget {
   const GradientContiner(this.color,{super.key});
+
+  void rollDice() {
+
+  }
 
   final List<Color> color ;
   @override
@@ -16,8 +21,13 @@ class GradientContiner extends StatelessWidget {
         end: Alignment.topCenter,
       )),
       child: Center(
-        child: Image.asset('assets/images/dice-2.png',width: 200,) ,
-      ),
+        child: Column(children: [
+          Image.asset(
+            'assets/images/dice-2.png',
+            width: 200,
+            ) ,
+          ElevatedButton(onPressed: rollDice, child: const StyleText('Press Here'))
+      ]),) 
     );
   }
 }
