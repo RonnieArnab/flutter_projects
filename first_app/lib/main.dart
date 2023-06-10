5b38e3035a76286eabd9d9ff554f790ae83fdc7e
Arnab Ghosh <as.920037arnabghosh@gmail.com>
2023-06-10 15:19:18 +0530

    changes background color to gradient

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( 
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+            colors: [Color(0xffd8fbff), Color(0xff45a7f5)],
+            stops: [0.4, 1],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          )
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
