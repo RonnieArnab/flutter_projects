5ab4bc7ab4a47677b6c19ad69ca173bbf0cf19e6
Arnab Ghosh <as.920037arnabghosh@gmail.com>
2023-06-10 20:55:54 +0530

    final update

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,13 +1,10 @@
-import 'package:first_app/style_text.dart';
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-// import 'package:first_app/style_text.dart' ;
+
 
 class GradientContiner extends StatelessWidget {
   const GradientContiner(this.color,{super.key});
-
-  void rollDice() {
-
-  }
+  
 
   final List<Color> color ;
   @override
@@ -20,14 +17,9 @@ class GradientContiner extends StatelessWidget {
         begin: Alignment.bottomCenter,
         end: Alignment.topCenter,
       )),
-      child: Center(
-        child: Column(children: [
-          Image.asset(
-            'assets/images/dice-2.png',
-            width: 200,
-            ) ,
-          ElevatedButton(onPressed: rollDice, child: const StyleText('Press Here'))
-      ]),) 
+      child:const Center(
+        child:  DiceRoller() ,
+      ) 
     );
   }
 }
