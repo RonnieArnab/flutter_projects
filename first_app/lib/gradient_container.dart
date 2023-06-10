ef51b911d815799264673b325dde6b6d887aabf0
Arnab Ghosh <as.920037arnabghosh@gmail.com>
2023-06-10 18:57:30 +0530

    custom constructor

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:first_app/style_text.dart' ;
 
 class GradientContiner extends StatelessWidget {
-  const GradientContiner({super.key});
+  const GradientContiner(this.color,{super.key});
+
+  final List<Color> color ;
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration:  BoxDecoration(
           gradient: LinearGradient(
-        colors: [Color(0xffd8fbff), Color(0xff45a7f5)],
-        stops: [0.4, 1],
+        colors: color,
+        stops: const[0.4, 1],
         begin: Alignment.bottomCenter,
         end: Alignment.topCenter,
       )),
       child: const Center(
-        child: StyleText() ,
+        child: StyleText('Arnab') ,
       ),
     );
   }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import  'package:first_app/gradient_container.dart';
 
+const List<Color> color = [Color(0xffc8e7ca), Color(0xff8bc34a)];
+
 void main() {
   runApp( 
      const MaterialApp(
       home: Scaffold(
-        body: GradientContiner() ,
+        body: GradientContiner(color) ,
         ),
     ),
   );
